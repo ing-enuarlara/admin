3191f1b4ac9ce769f1cdb946e2e3ed0b3a5dab3e
Enuar Lara <enuar2110@gmail.com>
2022-11-05 01:29:11 -0500

    Cambio en version

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <strong>Copyright &copy; 2022 <a href="https://ing-enuarlara.co">@ing_enuarlara.co®™</a>.</strong>All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0.0
+        <b>Version</b> ENU-1.0.0
     </div>
 </footer>
