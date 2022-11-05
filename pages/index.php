28942cf45a6010df254b8f1e1334e2b2c4d3cd52
Enuar Lara <enuar2110@gmail.com>
2022-11-05 02:25:14 -0500

    Se modifica llamado del logo

--- pages/index.php
+++ pages/index.php
@@ -9,7 +9,7 @@
   <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
       <div class="preloader flex-column justify-content-center align-items-center">
-        <img class="animation__shake" src="../dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        <img class="animation__shake" src="../dist/img/<?=$configuracion['conf_logo']?>" alt="AdminZEFELogo" height="60" width="60">
       </div>
 
       <?php include("../includes/encabezado.php"); ?>
