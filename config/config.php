4b497a147aa4c44525dda92811bb5a1c4a26c8f9
Enuar Lara <enuar2110@gmail.com>
2022-11-06 04:28:03 -0500

    Se agrega array tipoCRUD

--- config/config.php
+++ config/config.php
@@ -1,3 +1,5 @@
 <?php
 $consultaConfig = $conexionBdGeneral->query("SELECT * FROM configuracion WHERE conf_id=1");
 $configuracion = mysqli_fetch_array($consultaConfig, MYSQLI_BOTH);
+
+$tipoCrud= array("", "Create", "Read", "Update", "Delete");
