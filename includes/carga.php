d49d119196d7b3e4c13c334dff2245951c8737a5
Enuar Lara <enuar2110@gmail.com>
2022-11-06 01:08:42 -0500

    Se crea archivo de espera

---
+++ includes/carga.php
@@ -0,0 +1,4 @@
+<?php $urlLogo=REDIRECT_ROUTE.'/dist/img/logo.png';?>
+<div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="<?=$urlLogo?>" alt="AdminZEFELogo" height="80" width="80">
+</div>
