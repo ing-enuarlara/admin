670f1797d0da72494aa773e840b036542ec01ac0
Enuar Lara <enuar2110@gmail.com>
2022-11-13 18:00:47 -0500

    Se crea CRUD para marcas

---
+++ modules/comercial/bd_create/marcas-guardar.php
@@ -0,0 +1,14 @@
+<?php
+    require_once("../../sesion.php");
+
+    $idPagina = 34;
+    include(RUTA_PROYECTO."includes/verificar-paginas.php");
+    
+    $conexionBdComercial->query("INSERT INTO comercial_marcas(cmar_nombre, cmar_categoria, cmar_id_empresa)VALUES('" . $_POST["nombre"] . "', '" . $_POST["categoria"] . "', '" . $datosUsuarioActual['usr_id_empresa'] . "')");
+
+    $idInsertU = mysqli_insert_id($conexionBdComercial);
+
+    include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
+
+    echo '<script type="text/javascript">window.location.href="../bd_read/marcas-editar.php?id=' . $idInsertU . '";</script>';
+    exit();
---
+++ modules/comercial/bd_update/marcas-actualizar.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("../../sesion.php");
+
+    $idPagina = 36;
+    include(RUTA_PROYECTO."includes/verificar-paginas.php");
+    
+    $conexionBdComercial->query("UPDATE comercial_marcas SET cmar_nombre='" . $_POST["nombre"] . "', cmar_categoria='" . $_POST["categoria"] . "', cmar_id_empresa='" . $datosUsuarioActual['usr_id_empresa'] . "' WHERE cmar_id='" . $_POST["id"] . "'");
+
+    include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
+
+    echo '<script type="text/javascript">window.location.href="../bd_read/marcas.php";</script>';
+    exit();
