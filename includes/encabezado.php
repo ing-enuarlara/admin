2a5fb736b876656321af6da216698ddc76cf8222
Enuar Lara <enuar2110@gmail.com>
2022-11-13 22:44:29 -0500

    Se incluye enlace a tienda

--- includes/encabezado.php
+++ includes/encabezado.php
@@ -8,7 +8,7 @@
     <a href="index.php" class="nav-link">Home</a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
-    <a href="#" class="nav-link">Contact</a>
+    <a href="<?=REDIRECT_ROUTE?>../tercer-proyecto-podcalopers/podcalopers" class="nav-link" target="_target">Tienda</a>
     </li>
 </ul>
 
