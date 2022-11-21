92405615e7280a00a040e038d028b8909dec09c0
Enuar Lara <enuar2110@gmail.com>
2022-11-21 17:46:05 -0500

    Se corrige error de auto_increment

--- modules/administrativo/bd_delete/roles-eliminar.php
+++ modules/administrativo/bd_delete/roles-eliminar.php
@@ -4,9 +4,7 @@
 	$idPagina = 47;
     include(RUTA_PROYECTO."includes/verificar-paginas.php");
 
-    $autoId=($_GET["id"] - 1);
 	$conexionBdAdministrativo->query("DELETE FROM administrativo_roles WHERE utipo_id='" . $_GET["id"] . "'");
-	$conexionBdAdministrativo->query("ALTER TABLE administrativo_roles AUTO_INCREMENT = " . $autoId);
 
 	include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
 
