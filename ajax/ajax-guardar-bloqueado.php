5370056f997d529f7e1eade272d95b5b56cfe574
Enuar Lara <enuar2110@gmail.com>
2022-11-21 21:32:52 -0500

    Se crea archivo ajax para guardar estado bloqueado

---
+++ ajax/ajax-guardar-bloqueado.php
@@ -0,0 +1,44 @@
+<?php include("../modules/sesion.php");?>
+<?php //include("verificar-carga.php");?>
+<?php
+$mensajeNot = 'Hubo un error al guardar las cambios';
+//Bloquear y desbloquear
+if($_POST["operacion"]==1){
+	$conexionBdAdministrativo->query("UPDATE administrativo_usuarios SET usr_bloqueado='".$_POST["valor"]."' WHERE usr_id='".$_POST["idR"]."'");
+	$mensajeNot = 'El usuario ha cambiado de estado correctamente.';
+}
+?>
+
+<script type="text/javascript">
+function notifica(){
+	$.toast({
+		heading: 'Cambios guardados',  
+		text: '<?=$mensajeNot;?>',
+		position: 'botom-left',
+		loaderBg:'#ff6849',
+		icon: 'success',
+		hideAfter: 3000, 
+		stack: 6
+	});
+}
+setTimeout ("notifica()", 100);
+</script>
+
+<?php 
+if($_POST["operacion"]<1){
+?>
+<div class="alert alert-success">
+	<button type="button" class="close" data-dismiss="alert">&times;</button>
+	<i class="icon-exclamation-sign"></i><strong>INFORMACI&Oacute;N:</strong> <?=$mensajeNot;?>
+</div>
+<?php }?>
+
+<?php 
+if($_POST["operacion"]==2){
+?>
+	<script type="text/javascript">
+	setTimeout('document.location.reload()',2000);
+	</script>
+<?php
+}
+?>
