9f67adb4e8c16147ef4b15857a07264de9ec2aca
Enuar Lara <enuar2110@gmail.com>
2022-12-24 13:24:44 -0500

    Se agregan conexiones

--- conexion.php
+++ conexion.php
@@ -1,20 +1,20 @@
 <?php
-const SERVER = 'localhost' ;
-const USER = 'root';
-const PASS = 'zefe07EL';
-const BDADMIN = 'adminzefe_admin';
-const BDGENERAL = 'adminzefe_general';
-const BDMODADMINISTRATIVO= 'adminzefe_modulo_administrativo';
-const BDMODCOMERCIAL= 'adminzefe_modulo_comercial';
-const BDMODMICUENTA= 'adminzefe_modulo_micuenta';
-const BDMODSISTEMA= 'adminzefe_modulo_sistema';
+const SERVER = 'localhost' ;#SERVIDOR
+const USER = 'root';#USUARIO
+const PASS = 'zefe07EL';#CONTRASEÑA
+const BDADMIN = 'adminzefe_admin';#BASE DE DATOS ADMINISTRATIVA
+const BDGENERAL = 'adminzefe_general';#BASE DE DATOS GENERAL
+const BDMODADMINISTRATIVO= 'adminzefe_modulo_administrativo';#BASE DE DATOS MODULOS ADMINISTRATIVO
+const BDMODCOMERCIAL= 'adminzefe_modulo_comercial';#BASE DE DATOS MODULO COMERCIAL
+const BDMODMICUENTA= 'adminzefe_modulo_micuenta';#BASE DE DATOS MODULO MI CUENTA
+const BDMODSISTEMA= 'adminzefe_modulo_sistema';#BASE DE DATOS MODULO DEL SISTEMA
 
-$conexionBdAdmin = new mysqli(SERVER, USER, PASS, BDADMIN);
-$conexionBdGeneral = new mysqli(SERVER, USER, PASS, BDGENERAL);
-$conexionBdAdministrativo = new mysqli(SERVER, USER, PASS, BDMODADMINISTRATIVO);
-$conexionBdComercial = new mysqli(SERVER, USER, PASS, BDMODCOMERCIAL);
-$conexionBdMicuenta = new mysqli(SERVER, USER, PASS, BDMODMICUENTA);
-$conexionBdSistema = new mysqli(SERVER, USER, PASS, BDMODSISTEMA);
+$conexionBdAdmin = new mysqli(SERVER, USER, PASS, BDADMIN);#CONEXION BD ADMINISTRATIVA
+$conexionBdGeneral = new mysqli(SERVER, USER, PASS, BDGENERAL);#CONEXION BD GENERAL
+$conexionBdAdministrativo = new mysqli(SERVER, USER, PASS, BDMODADMINISTRATIVO);#CONEXION BD ADMINISTRATIVO
+$conexionBdComercial = new mysqli(SERVER, USER, PASS, BDMODCOMERCIAL);#CONEXION BD COMERCIAL
+$conexionBdMicuenta = new mysqli(SERVER, USER, PASS, BDMODMICUENTA);#CONEXION BD MI CUENTA
+$conexionBdSistema = new mysqli(SERVER, USER, PASS, BDMODSISTEMA);#CONEXION BD SISTEMA
 
-
-const REDIRECT_ROUTE = 'http://localhost/admin/';
+//RUTA DEL PROYECTO
+const REDIRECT_ROUTE = 'http://localhost/ing-enuarlara.co/admin/';
