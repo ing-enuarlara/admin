79b7f272eafd954000a5976f509b21f6a9ad4960
Enuar Lara <enuar2110@gmail.com>
2023-06-20 21:36:19 -0500

    Mejora en colores

--- modules/sistema/bd_update/configuracion-color-actualizar.php
+++ modules/sistema/bd_update/configuracion-color-actualizar.php
@@ -4,7 +4,16 @@
     $idPagina = 41;
     include(RUTA_PROYECTO."includes/verificar-paginas.php");
 
-    $conexionBdGeneral->query("UPDATE general_color_store SET gcs_primario='" . $_POST["colorP"] . "', gcs_primarioOscuro='" . $_POST["colorPO"] . "', gcs_secundario='" . $_POST["colorS"] . "', gcs_secundarioOscuro='" . $_POST["colorSO"] . "', gcs_blanco='" . $_POST["colorC"] . "', gcs_negro='" . $_POST["colorO"] . "' WHERE gcs_id_empresa='" . $_POST["id"] . "'");
+    $consultaConfigColor = $conexionBdGeneral->query("SELECT * FROM general_color_store WHERE gcs_id_empresa='".$configuracion['conf_id_empresa']."'");
+    $numDatos= $consultaConfigColor->num_rows;
+
+    if($numDatos>0){
+        
+        $conexionBdGeneral->query("UPDATE general_color_store SET gcs_primario='" . $_POST["colorP"] . "', gcs_primarioOscuro='" . $_POST["colorPO"] . "', gcs_secundario='" . $_POST["colorS"] . "', gcs_secundarioOscuro='" . $_POST["colorSO"] . "', gcs_blanco='" . $_POST["colorC"] . "', gcs_negro='" . $_POST["colorO"] . "' WHERE gcs_id_empresa='" . $_POST["id"] . "'");
+    }else{
+
+        $conexionBdGeneral->query("INSERT INTO general_color_store (gcs_primario, gcs_primarioOscuro, gcs_secundario, gcs_secundarioOscuro, gcs_blanco, gcs_negro, gcs_id_empresa) VALUES ('" . $_POST["colorP"] . "', '" . $_POST["colorPO"] . "', '" . $_POST["colorS"] . "', '" . $_POST["colorSO"] . "', '" . $_POST["colorC"] . "', '" . $_POST["colorO"] . "', '" . $_POST["id"] . "')");
+    }
 
     include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
 
