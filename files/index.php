0677995b8afd6266812a6d4abe6e3389954ec441
Enuar Lara <enuar2110@gmail.com>
2023-06-22 02:28:49 -0500

    Se elimana files

--- files/index.php
+++
@@ -1,8 +0,0 @@
-<span style='font-family:Arial;'>No tienes permiso para entrar aqui.</samp>
-
-<script type="text/javascript">
-function sacar(){
-    window.location.href="../salir.php";
-} 
-setInterval('sacar()', 3000);
-</script>
