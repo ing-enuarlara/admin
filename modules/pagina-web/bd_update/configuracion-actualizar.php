702a9eba68f78de348dd936a518894881ec4e2ec
Enuar Lara <enuar2110@gmail.com>
2023-06-24 23:17:54 -0500

    Mejora para titulo de la pagina

--- modules/pagina-web/bd_update/configuracion-actualizar.php
+++ modules/pagina-web/bd_update/configuracion-actualizar.php
@@ -86,7 +86,7 @@
 		$whatsapp='https://api.whatsapp.com/send?phone=57'.$_POST["whatsapp"];
 	}
 
-    $conexionBdPaginaWeb->query("UPDATE configuracion SET conf_empresa='" . $_POST["nombre"] . "', conf_descripcion_pagina='" . $_POST["descripcionPagina"] . "', conf_palabras_claves='" . $_POST["palabrasClaves"] . "', conf_email='" . $_POST["email"] . "', conf_telefono='" . $_POST["telefono"] . "', conf_web='" . $_POST["web"] . "', conf_direccion='" . $_POST["direccion"] . "', conf_envios='" . $_POST["envios"] . "', conf_text_encabezado='" . $_POST["textEncabezado"] . "', conf_facebook='" . $_POST["facebook"] . "', conf_instagram='" . $_POST["instagram"] . "', conf_tiktok='" . $_POST["tiktok"] . "', conf_whatsapp='" . $whatsapp . "' WHERE conf_id_empresa='" . $_POST["id"] . "'");
+    $conexionBdPaginaWeb->query("UPDATE configuracion SET conf_empresa='" . $_POST["nombre"] . "', conf_descripcion_pagina='" . $_POST["descripcionPagina"] . "', conf_descripcion_corta='" . $_POST["descripcionCorta"] . "', conf_palabras_claves='" . $_POST["palabrasClaves"] . "', conf_email='" . $_POST["email"] . "', conf_telefono='" . $_POST["telefono"] . "', conf_web='" . $_POST["web"] . "', conf_direccion='" . $_POST["direccion"] . "', conf_envios='" . $_POST["envios"] . "', conf_text_encabezado='" . $_POST["textEncabezado"] . "', conf_facebook='" . $_POST["facebook"] . "', conf_instagram='" . $_POST["instagram"] . "', conf_tiktok='" . $_POST["tiktok"] . "', conf_whatsapp='" . $whatsapp . "' WHERE conf_id_empresa='" . $_POST["id"] . "'");
 
     include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
 
