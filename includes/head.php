41a96dc7042699c57e9cb52a381dab577a2be2c1
Enuar Lara <enuar2110@gmail.com>
2023-06-25 15:49:40 -0500

    Mejora para operaciones Jquery

--- includes/head.php
+++ includes/head.php
@@ -5,3 +5,4 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminZEFE 1.0 | <?=$paginaActual['pag_nombre']?></title>
     <script src="https://kit.fontawesome.com/e84fa1cf78.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
