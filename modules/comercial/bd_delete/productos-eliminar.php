c00c11f4650d9198389cacff5bbfb72f59a63a53
Enuar Lara <enuar2110@gmail.com>
2023-06-25 21:22:20 -0500

    Se eliminan fotos al eliminar un productos

--- modules/comercial/bd_delete/productos-eliminar.php
+++ modules/comercial/bd_delete/productos-eliminar.php
@@ -5,6 +5,7 @@
     include(RUTA_PROYECTO."includes/verificar-paginas.php");
 
 	$conexionBdComercial->query("DELETE FROM comercial_productos WHERE cprod_id='" . $_GET["id"] . "'");
+	$conexionBdComercial->query("DELETE FROM comercial_productos_fotos WHERE cpf_id_producto='" . $_GET["id"] . "'");
 
 	include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
 
