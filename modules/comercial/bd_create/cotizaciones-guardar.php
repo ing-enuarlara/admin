120e09f8497e973f10b60f71192bdd2b1c55e31d
Enuar Lara <enuar2110@gmail.com>
2023-07-26 01:43:07 -0500

    Se crea script para guardar cotizaciones en BD

---
+++ modules/comercial/bd_create/cotizaciones-guardar.php
@@ -0,0 +1,16 @@
+<?php
+    require_once("../../sesion.php");
+
+    $idPagina = 78;
+    include(RUTA_PROYECTO."includes/verificar-paginas.php");
+    
+    $conexionBdComercial->query("INSERT INTO comercial_cotizaciones(cotiz_fecha_propuesta, cotiz_cliente, cotiz_fecha_vencimiento, cotiz_vendedor, cotiz_creador, cotiz_forma_pago, cotiz_fecha_creacion, cotiz_moneda, cotiz_observaciones, cotiz_id_empresa)VALUES('" . $_POST["fechaPropuesta"] . "','" . $_POST["cliente"] . "','" . $_POST["fechaVencimiento"] . "','" . $_POST["vendedor"] . "','" . $_SESSION["id"] . "','" . $_POST["formaPago"] . "',now(),'" . $_POST["moneda"] . "','" . $_POST["notas"] . "', '" . $datosUsuarioActual['usr_id_empresa'] . "')");
+
+    $idInsertU = mysqli_insert_id($conexionBdComercial);
+
+    require("guardar-productos-cotizacion.php");
+
+    include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
+
+    echo '<script type="text/javascript">window.location.href="../bd_read/cotizaciones.php?id=' . $idInsertU . '";</script>';
+    exit();
---
+++ modules/comercial/bd_create/guardar-productos-cotizacion.php
@@ -0,0 +1,22 @@
+<?php
+//Productos
+if(!empty($_POST["producto"])){
+    $numProductos = (count($_POST["producto"]));
+
+    if ($numProductos > 0):
+        $contProducto = 0;
+        while ($contProducto < $numProductos):
+
+            $consulta=$conexionBdComercial->query("SELECT * FROM comercial_productos WHERE cprod_id='" . $_POST["producto"][$contProducto] . "'");
+            $productoDatos = mysqli_fetch_array($consulta, MYSQLI_BOTH);
+
+            $valorProducto = $productoDatos['cprod_costo'];
+
+            $conexionBdComercial->query("INSERT INTO comercial_relacion_productos(czpp_cotizacion, czpp_producto, czpp_valor, czpp_orden, czpp_cantidad, czpp_impuesto, czpp_tipo)VALUES('" . $idInsertU . "','" . $_POST["producto"][$contProducto] . "', '" . $valorProducto . "', '" . $numProductos . "', 1, 19, 1)");
+
+            $contProducto++;
+
+        endwhile;
+
+    endif;
+}
