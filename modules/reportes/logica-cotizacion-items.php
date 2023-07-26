26a45e50690aa118f96449b97b2a2f00ad48b4ea
Enuar Lara <enuar2110@gmail.com>
2023-07-26 01:44:55 -0500

    Se crea formato para imprimir unacotización

---
+++ modules/reportes/logica-cotizacion-items.php
@@ -0,0 +1,27 @@
+<?php
+$dcto = 0;
+$valorTotal = 0;
+
+if($prod['czpp_valor']!=''){
+	$valorTotal = $prod['czpp_valor'] * $prod['czpp_cantidad'];
+}else{
+	$valorTotal = 0 * $prod['czpp_cantidad'];
+}
+
+if($prod['czpp_cantidad']>0 and $prod['czpp_descuento']>0){
+	$dcto = ($valorTotal * ($prod['czpp_descuento']/100));
+	$totalDescuento += $dcto;	
+}
+
+$valorConDcto = $valorTotal - $dcto;
+$totalIva +=($valorConDcto * ($prod['czpp_impuesto']/100));
+
+$subtotal +=$valorTotal;
+
+
+$totalCantidad = $prod['czpp_cantidad'];
+
+$fondo = 'white';
+if ($no % 2 == 0) {
+	$fondo = '#f8f9fa';
+}
