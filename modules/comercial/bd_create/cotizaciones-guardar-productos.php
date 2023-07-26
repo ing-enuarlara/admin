51fb35b8cf66bdc974ad62d3383d25e96732b191
Enuar Lara <enuar2110@gmail.com>
2023-07-26 16:20:08 -0500

    Se ñade para cambio de moneda

--- modules/comercial/bd_create/cotizaciones-guardar-productos.php
+++ modules/comercial/bd_create/cotizaciones-guardar-productos.php
@@ -10,7 +10,12 @@ if(!empty($_POST["producto"])){
             $consulta=$conexionBdComercial->query("SELECT * FROM comercial_productos WHERE cprod_id='" . $_POST["producto"][$contProducto] . "'");
             $productoDatos = mysqli_fetch_array($consulta, MYSQLI_BOTH);
 
+            //Para pesos colombianos
             $valorProducto = $productoDatos['cprod_costo'];
+            //Si la cotización ya está en USD
+            if ($_POST["moneda"] == 2) {
+                $valorProducto = round(($productoDatos['cprod_costo']/$precioDolarCOP),2);
+            }
 
             $conexionBdComercial->query("INSERT INTO comercial_relacion_productos(czpp_cotizacion, czpp_producto, czpp_valor, czpp_orden, czpp_cantidad, czpp_impuesto, czpp_tipo)VALUES('" . $idInsertU . "','" . $_POST["producto"][$contProducto] . "', '" . $valorProducto . "', '" . $numProductos . "', 1, 19, 1)");
 
