6d64e8c03ec8ecee7523591b32e4c79a438d35a2
Enuar Lara <enuar2110@gmail.com>
2023-07-26 20:21:24 -0500

    Mejora en redireccionamiento

--- apis/index.php
+++ apis/index.php
@@ -1,8 +1,39 @@
-<span style='font-family:Arial;'>No tienes permiso para entrar aqui.</samp>
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT']."/ing-enuarlara.co/admin/constantes.php");
+    $rutaSalida= REDIRECT_ROUTE."salir.php";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="2;url=<?= $rutaSalida ?>">
+    <title>Redireccionando...</title>
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #FAFAFA;
+            /* font-family: Arial, sans-serif; */
+        }
 
-<script type="text/javascript">
-function sacar(){
-    window.location.href="../salir.php";
-} 
-setInterval('sacar()', 3000);
-</script>
+        .mensaje {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="mensaje">
+        <img src="<?=REDIRECT_ROUTE?>files/logo/logo.png" alt="AdminZEFELogo" width="100">
+        <p><span style='font-family:Arial; font-weight:bold;'>No tienes permiso para entrar aqui.</samp></p>
+        <p>Redireccionando en 2 segundos...</p>
+    </div>
+    <script>
+        setTimeout(function() {
+            window.location.href = '<?= $rutaSalida ?>';
+        }, 2000);
+    </script>
+</body>
+</html>
--- includes/sesion-usuario-actual.php
+++ includes/sesion-usuario-actual.php
@@ -13,14 +13,43 @@
 	//SABER SI ESTA BLOQUEADO
 	if($datosUsuarioActual['usr_bloqueado']==1)
 	{
-?>		
-		<span style='font-family:Arial; color:red;'>Su usuario ha sido bloqueado. Por tanto no tiene permisos para acceder al Sistema.</samp>
-        <script type="text/javascript">
-		function sacar(){
-			window.location.href="../salir.php";
-		} 
-		setInterval('sacar()', 3000);
-        </script>
+		$rutaSalida= REDIRECT_ROUTE."salir.php";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="2;url=<?= $rutaSalida ?>">
+    <title>Redireccionando...</title>
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #FAFAFA;
+            /* font-family: Arial, sans-serif; */
+        }
+
+        .mensaje {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="mensaje">
+        <img src="<?=REDIRECT_ROUTE?>files/logo/logo.png" alt="AdminZEFELogo" width="100">
+        <p><span style='font-family:Arial; color:red; font-weight:bold;'>Su usuario ha sido bloqueado. Por tanto no tiene permisos para acceder al Sistema.</samp></p>
+        <p>Redireccionando en 2 segundos...</p>
+    </div>
+    <script>
+        setTimeout(function() {
+            window.location.href = '<?= $rutaSalida ?>';
+        }, 2000);
+    </script>
+</body>
+</html>
 <?php	
-    	exit();	
-	}
+	exit();	
+}
