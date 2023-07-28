8c7368bdaad9344e8350926cb02449c05f4b4f7c
Enuar Lara <enuar2110@gmail.com>
2023-07-28 01:03:11 -0500

    Se crea script para eliminar pedidos

---
+++ modules/comercial/bd_delete/pedidos-eliminar.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("../../sesion.php");
+
+    $idPagina = 92;
+    include(RUTA_PROYECTO."includes/verificar-paginas.php");
+
+    mysqli_query($conexionBdComercial,"DELETE FROM comercial_pedidos WHERE pedid_id='" . $_GET["id"] . "'");
+    mysqli_query($conexionBdComercial,"DELETE FROM comercial_pedidos_novedades WHERE pednov_pedido='" . $_GET["id"] . "'");
+	mysqli_query($conexionBdComercial,"DELETE FROM comercial_relacion_productos WHERE czpp_cotizacion='" . $_GET["id"] . "' AND czpp_tipo=2");
+    mysqli_query($conexionBdComercial,"UPDATE comercial_cotizaciones SET cotiz_vendida=0 WHERE cotiz_id='" . $_GET["idC"] . "'");
+
+    include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
+
+    echo '<script type="text/javascript">window.location.href="../bd_read/pedidos.php";</script>';
+    exit();
