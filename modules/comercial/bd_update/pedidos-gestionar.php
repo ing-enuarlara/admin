f211368ba1f354e48da88b4c1a0fbf08ea86040e
Enuar Lara <enuar2110@gmail.com>
2023-07-28 01:02:31 -0500

    Se crea script para anular o restaurar pedido

---
+++ modules/comercial/bd_update/pedidos-gestionar.php
@@ -0,0 +1,30 @@
+<?php
+    require_once("../../sesion.php");
+
+    $idPagina = 91;
+    include(RUTA_PROYECTO."includes/verificar-paginas.php");
+
+    if($_GET["op"]==1){
+        mysqli_query($conexionBdComercial,"UPDATE comercial_pedidos SET pedid_estado=0 WHERE pedid_id='" . $_GET["id"] . "'");
+
+        $dia=date("d");
+        $mes=date("M");
+        mysqli_query($conexionBdComercial,"INSERT INTO comercial_pedidos_novedades(pednov_dia, pednov_mes, pednov_estado, pednov_novedad, pednov_pedido, pednov_usuario)VALUES('" . $dia . "', '" . $mes . "', 0, 'Su pedido fue anulado', '" . $_GET["id"] . "', '" . $_SESSION["id"] . "')");
+
+        mysqli_query($conexionBdComercial,"UPDATE comercial_cotizaciones SET cotiz_vendida=0 WHERE cotiz_id='" . $_GET["idC"] . "'");
+    }
+
+    if($_GET["op"]==2){
+        mysqli_query($conexionBdComercial,"UPDATE comercial_pedidos SET pedid_estado=1 WHERE pedid_id='" . $_GET["id"] . "'");
+
+        $dia=date("d");
+        $mes=date("M");
+        mysqli_query($conexionBdComercial,"INSERT INTO comercial_pedidos_novedades(pednov_dia, pednov_mes, pednov_estado, pednov_novedad, pednov_pedido, pednov_usuario)VALUES('" . $dia . "', '" . $mes . "', 1, 'Su pedido fue restaurado, lo estamos preparando para su envío', '" . $_GET["id"] . "', '" . $_SESSION["id"] . "')");
+
+        mysqli_query($conexionBdComercial,"UPDATE comercial_cotizaciones SET cotiz_vendida=1 WHERE cotiz_id='" . $_GET["idC"] . "'");
+    }
+
+    include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
+
+    echo '<script type="text/javascript">window.location.href="../bd_read/pedidos.php";</script>';
+    exit();
