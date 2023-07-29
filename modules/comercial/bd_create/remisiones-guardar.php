f0b08314dea4aad54a2e5f78832b00baf4f88cbe
Enuar Lara <enuar2110@gmail.com>
2023-07-28 22:01:02 -0500

    Se crea script para guardar nueva remisión en BD

---
+++ modules/comercial/bd_create/remisiones-guardar.php
@@ -0,0 +1,18 @@
+<?php
+    require_once("../../sesion.php");
+    require('../../../apis/apis-dolar.php');
+
+    $idPagina = 97;
+    include(RUTA_PROYECTO."includes/verificar-paginas.php");
+    
+    $conexionBdComercial->query("INSERT INTO comercial_remisiones(remi_fecha_propuesta, remi_cliente, remi_fecha_vencimiento, remi_vendedor, remi_creador, remi_forma_pago, remi_fecha_creacion, remi_moneda, remi_observaciones, remi_id_empresa)VALUES('" . $_POST["fechaPropuesta"] . "','" . $_POST["cliente"] . "','" . $_POST["fechaVencimiento"] . "','" . $_POST["vendedor"] . "','" . $_SESSION["id"] . "','" . $_POST["formaPago"] . "',now(),'" . $_POST["moneda"] . "','" . $_POST["notas"] . "', '" . $datosUsuarioActual['usr_id_empresa'] . "')");
+
+    $idInsertU = mysqli_insert_id($conexionBdComercial);
+
+    $tipo = 3;
+    require("cotizaciones-guardar-productos.php");
+
+    include(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
+
+    echo '<script type="text/javascript">window.location.href="../bd_read/remisiones-editar.php?id=' . $idInsertU . '";</script>';
+    exit();
