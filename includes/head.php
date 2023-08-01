cb5aeff588d7c5c9cd3a0e1aef64967aab58071a
Enuar Lara <enuar2110@gmail.com>
2023-07-31 19:19:02 -0500

    Mejora dinamica para el titulo de la pagina

--- includes/head.php
+++ includes/head.php
@@ -6,3 +6,12 @@
     <title>AdminZEFE 1.0 | <?=$paginaActual['pag_nombre']?></title>
     <script src="https://kit.fontawesome.com/e84fa1cf78.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript">
+        let docTitle = document.title;
+        window.addEventListener("blur", ()=>{
+          document.title="Regresa Pronto ;)";
+        });
+        window.addEventListener("focus", ()=>{
+          document.title=docTitle;
+        });
+    </script>
