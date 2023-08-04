5a5e927e1d70edc0e3e362b419b1a7e8be1fb431
Enuar Lara <enuar2110@gmail.com>
2023-08-03 22:13:44 -0500

    Mejora para constantes V2

--- constantes.php
+++ constantes.php
@@ -1,18 +1,14 @@
 <?php
-define('RUTA_PROYECTO', $_SERVER['DOCUMENT_ROOT'].'/ing-enuarlara.co/admin/');
-require_once(RUTA_PROYECTO."/sensitive.php");
-
-define('EMAIL_SENDER', 'enuar2110@gmail.com');
-define('NAME_SENDER', '@ing_enuarlara.co');
-
 switch($_SERVER['HTTP_HOST']){
 	case 'localhost':
+        define('RUTA_PROYECTO', $_SERVER['DOCUMENT_ROOT'].'/ing-enuarlara.co/admin/');
         define('REDIRECT_ROUTE', 'http://localhost/ing-enuarlara.co/admin/');
-        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         break;
 
 	// case 'dominio.com.co':
+        // define('RUTA_PROYECTO', '/home4/nose/public_html/dominio.com.co');
         // define('REDIRECT_ROUTE', 'https://dominio.com.co');
         // error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         // break;
 }
+include(RUTA_PROYECTO."sensitive.php");
