18d5f4344e18f118561c325e4ae20c2924ba0fc1
Enuar Lara <enuar2110@gmail.com>
2023-08-03 22:00:56 -0500

    Mejora en constantes

--- constantes.php
+++ constantes.php
@@ -1,13 +1,18 @@
 <?php
+define('RUTA_PROYECTO', $_SERVER['DOCUMENT_ROOT'].'/ing-enuarlara.co/admin/');
+require_once(RUTA_PROYECTO."/sensitive.php");
+
+define('EMAIL_SENDER', 'enuar2110@gmail.com');
+define('NAME_SENDER', '@ing_enuarlara.co');
+
 switch($_SERVER['HTTP_HOST']){
 	case 'localhost':
-        define('RUTA_PROYECTO', 'C:/xampp/htdocs/ing-enuarlara.co/admin/');
         define('REDIRECT_ROUTE', 'http://localhost/ing-enuarlara.co/admin/');
+        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         break;
 
-	// case 'nuevo.orioncrm.com.co':
-        // define('RUTA_PROYECTO', '/home4/orioncrmcom/public_html/nuevo.orioncrm.com.co/softjmgit');
-        // define('REDIRECT_ROUTE', 'https://nuevo.orioncrm.com.co/softjmgit');
+	// case 'dominio.com.co':
+        // define('REDIRECT_ROUTE', 'https://dominio.com.co');
         // error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         // break;
 }
