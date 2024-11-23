5c09590137e973c6b07d1b7de8944fec5f4357d9
ing-enuarlara <enuar2110@gmail.com>
2024-11-23 20:15:19 +0100

    se modifica archivo de constantes

--- constantes.php
+++ constantes.php
@@ -6,11 +6,11 @@ switch($_SERVER['HTTP_HOST']){
         error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         break;
 
-	// case 'dominio.com.co':
-        // define('RUTA_PROYECTO', '/home4/nose/public_html/dominio.com.co');
-        // define('REDIRECT_ROUTE', 'https://dominio.com.co');
-        // error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
-        // break;
+        case 'adminzefe.ing-enuarlara.com';
+        define('RUTA_PROYECTO', '/home3/ingenuar/adminzefe.ing-enuarlara.com');
+        define('REDIRECT_ROUTE', 'https://adminzefe.ing-enuarlara.com');
+        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
+        break;
 }
 
 include(RUTA_PROYECTO."sensitive.php");
