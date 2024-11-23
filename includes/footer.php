a5affd7c444755a0f5ea8f49c6efa313aa57dfd3
ing-enuarlara <enuar2110@gmail.com>
2024-11-23 17:06:51 +0100

    Se cambia el footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     require_once(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
 ?>
 <footer class="main-footer">
-    <strong>Copyright &copy; <?=date("Y");?> <a href="https://ing-enuarlara.co">@ing_enuarlara.co®™</a>.</strong> Todos los Derechos son Reservados.
+    <strong>Copyright &copy; <?=date("Y");?> <a href="https://ing-enuarlara.com">@enuar_lara7®™</a>.</strong> Todos los Derechos son Reservados.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> ENU-1.0.0<br>
         <?php
