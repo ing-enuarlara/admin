0613bba1953269af2baffe33466ed0c6be4900bc
ing-enuarlara <enuar2110@gmail.com>
2025-01-28 12:32:04 +0100

    Se añade responsive a la barra del menu

--- includes/barra-developer.php
+++ includes/barra-developer.php
@@ -18,33 +18,44 @@
   }
   $ramaActual = substr($line, 16);
 ?>
+	<style>
+		.barra-developer{
+			position:relative;
+			background-color: #003832; 
+			color:#42FF00; 
+			height: 50px; 
+			width: 100%; 
+			margin-bottom: 20px; 
+			padding: 7px;
+			display:flex; 
+			justify-content: center; 
+			align-items: center;
+			font-family:Arial;
+			font-size:16px;
+		}
 
-	<div style="
-        position:relative;
-        background-color: #003832; 
-        color:#42FF00; 
-        height: 50px; 
-        width: 100%; 
-        margin-bottom: 20px; 
-        padding: 7px;
-        display:flex; 
-        justify-content: center; 
-        align-items: center;
-        font-family:Arial;
-        font-size:16px;
-    ">
-    <b>Rama Actual GIT:</b>&nbsp;<?=$ramaActual;?>&nbsp;|&nbsp;
-    <b>Id Pagina:</b>&nbsp;<?=$idPagina;?>&nbsp;|&nbsp;
-    <b>Usuario Actual:</b>&nbsp;<?=$datosUsuarioActual[0];?>&nbsp;|&nbsp;
-    <b>T. Usuario:</b>&nbsp;<?=$nombreUSR['utipo_nombre'];?>&nbsp;|&nbsp;
-		<b>V PHP:&nbsp;</b> <?=phpversion(); ?>&nbsp;|&nbsp; 
-		<b>IP:&nbsp;</b> <?=$_SERVER['REMOTE_ADDR'];?>&nbsp;|&nbsp; 
-		<!-- <b>Server:&nbsp;</b> <?=$_SERVER['SERVER_NAME']; ?>&nbsp;|&nbsp; -->
-		<b>Host:&nbsp;</b> <?=$_SERVER['HTTP_HOST']." (".http_response_code().")"; ?>&nbsp;|&nbsp;
-    <b>Peso Página:&nbsp;</b> <?=number_format(filesize($nombre_fichero)) . ' bytes'; ?>&nbsp;|&nbsp;
-    <b>ENV:&nbsp;</b> <?=ENVIROMENT;?>&nbsp;|&nbsp;
+		@media (max-width: 900px) {
+			.items-barra-developer{
+			display: none;
+			}
+		}
+	</style>
 
-    <?php if( isset($_SESSION['admin']) ){?>
+	<div class="barra-developer">
+		<div class="items-barra-developer">
+			<!-- <b>Rama Actual GIT:</b>&nbsp;<?=$ramaActual;?>&nbsp;|&nbsp; -->
+			<b>Id Pagina:</b>&nbsp;<?=$idPagina;?>&nbsp;|&nbsp;
+			<b>Usuario Actual:</b>&nbsp;<?=$datosUsuarioActual["usr_id"];?>&nbsp;|&nbsp;
+			<b>T. Usuario:</b>&nbsp;<?=$nombreUSR['utipo_nombre'];?>&nbsp;|&nbsp;
+			<b>V PHP:&nbsp;</b> <?=phpversion(); ?>&nbsp;|&nbsp; 
+			<b>IP:&nbsp;</b> <?=$_SERVER['REMOTE_ADDR'];?>&nbsp;|&nbsp; 
+			<!-- <b>Server:&nbsp;</b> <?=$_SERVER['SERVER_NAME']; ?>&nbsp;|&nbsp; -->
+			<b>Host:&nbsp;</b> <?=$_SERVER['HTTP_HOST']." (".http_response_code().")"; ?>&nbsp;|&nbsp;
+			<b>Peso Página:&nbsp;</b> <?=number_format(filesize($nombre_fichero)) . ' bytes'; ?>&nbsp;|&nbsp;
+			<b>ENV:&nbsp;</b> <?=ENVIROMENT;?>&nbsp;|&nbsp;
+		</div>
+
+		<?php if( isset($_SESSION['admin']) ){?>
 			<b>User Admin:&nbsp;</b> <?=$_SESSION['admin']; ?>&nbsp;|&nbsp;
 			<a href="<?=REDIRECT_ROUTE?>includes/return-admin-panel.php" style="color:white; text-decoration:underline;">RETORNAR</a>
 		<?php }?>
