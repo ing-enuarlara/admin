142041873411d613b82f236b299c728c182b6c19
ing-enuarlara <enuar2110@gmail.com>
2025-03-05 10:50:05 +0100

    Se actualiza footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,9 +2,9 @@
     require_once(RUTA_PROYECTO."includes/guardar-historial-acciones.php");
 ?>
 <footer class="main-footer">
-    <strong>Copyright &copy; <?=date("Y");?> <a href="https://ing-enuarlara.com">@enuar_lara7®™</a>.</strong> Todos los Derechos son Reservados.
+    <strong>Copyright &copy; <?=date("Y");?> <a href="https://dev.oceanblue.es" target="_blank">Ocean Blue Dev ®™</a>.</strong> Todos los Derechos son Reservados.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> ENU-1.0.0<br>
+        <b>Version</b> OCB-1.0.0<br>
         <?php
             if($datosUsuarioActual['usr_tipo']==DEV){
         ?>
