10356d31e380d83e6e2738f919a7ef6b7eeb8da0
ing-enuarlara <enuar2110@gmail.com>
2025-06-18 14:05:07 +0200

    Mejora para ajax de marcas

--- ajax/ajax-traer-sub-categorias.php
+++ ajax/ajax-traer-sub-categorias.php
@@ -1,43 +1,51 @@
 <?php
 include("../modules/sesion.php");
-try{
-    $consultaSubCategorias= $conexionBdComercial->query("SELECT * FROM comercial_marcas WHERE cmar_categoria='".$_POST["categoria"]."'");
-} catch (Exception $e) {
-    include(RUTA_PROYECTO."includes/error-catch-to-report.php");
-}
-$numSubCategorias=mysqli_num_rows($consultaSubCategorias);
+require_once(RUTA_PROYECTO . 'class/SubCategorias.php');
+require_once(RUTA_PROYECTO . 'class/Sub_Categorias.php');
+require_once(RUTA_PROYECTO . 'class/Clientes_Admin.php');
 
-if($numSubCategorias>0){
+SubCategorias::foreignKey(SubCategorias::INNER, [
+    "cmar_id" => 'subca_marca'
+]);
+$consultaSubCategorias = Sub_Categorias::SelectJoin(
+    [
+        "subca_cate" => $_POST["categoria"],
+        "subca_prin" => NO
+    ],
+    "cmar.*",
+    [
+        SubCategorias::class
+    ]
+);
+
+if (!empty($consultaSubCategorias)) {
 ?>
     <script type="application/javascript">
         document.getElementById('mensaje').style.display = "none";
         document.getElementById('subCategoria-container').style.display = "block";
-    </script> 
-<?php  
-    while($datosSubCategorias = mysqli_fetch_array($consultaSubCategorias, MYSQLI_BOTH)){
+    </script>
+    <?php
+    foreach ($consultaSubCategorias as $datosSubCategorias) {
 
-        $nombreEmpresa='';
-        if($_SESSION["datosUsuarioActual"]['usr_tipo']==DEV){
-            try{
-                $empresa= $conexionBdAdmin->query("SELECT * FROM clientes_admin WHERE cliAdmi_id='".$datosSubCategorias['cmar_id_empresa']."'");
-            } catch (Exception $e) {
-                include(RUTA_PROYECTO."includes/error-catch-to-report.php");
-            }
-            $nomEmpresa = mysqli_fetch_array($empresa, MYSQLI_BOTH);
-            $nombreEmpresa="[".$nomEmpresa['cliAdmi_nombre']."]";
+        $nombreEmpresa = '';
+        if ($_SESSION["datosUsuarioActual"]['usr_tipo'] == DEV) {
+            $empresa = Clientes_Admin::Select(['cliAdmi_id' => $datosSubCategorias['cmar_id_empresa']])->fetch(PDO::FETCH_ASSOC);
+            $nombreEmpresa = "[" . $empresa['cliAdmi_nombre'] . "]";
         }
-        $selected='';
-        if(!empty($_POST["subCategoria"]) && $_POST["subCategoria"]==$datosSubCategorias['cmar_id']){$selected='selected';}
+        $selected = '';
+        if (!empty($_POST["subCategoria"]) && $_POST["subCategoria"] == $datosSubCategorias['cmar_id']) {
+            $selected = 'selected';
+        }
 
-        echo '<option value="'.$datosSubCategorias['cmar_id'].'" '.$selected.'>'.$datosSubCategorias['cmar_nombre'].$nombreEmpresa.'</option>';
+        echo '<option value="' . $datosSubCategorias['cmar_id'] . '" ' . $selected . '>' . $datosSubCategorias['cmar_nombre'] . $nombreEmpresa . '</option>';
     }
     exit();
-}else{
-?>
+} else {
+    ?>
     <script type="application/javascript">
         document.getElementById('mensaje').style.display = "none";
         document.getElementById('subCategoria-container').style.display = "none";
         document.getElementById('marca').value = "0";
-    </script> 
-<?php    
+    </script>
+<?php
 }
