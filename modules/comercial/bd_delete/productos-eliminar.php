3841fa2f1e3edc5df03db5fd95489478374133fc
ing-enuarlara <enuar2110@gmail.com>
2025-07-03 18:23:07 +0200

    Se da opcion de eliminar

--- modules/comercial/bd_delete/productos-eliminar.php
+++ modules/comercial/bd_delete/productos-eliminar.php
@@ -4,6 +4,7 @@ require_once("../../sesion.php");
 $idPagina = 25;
 include(RUTA_PROYECTO . "includes/verificar-paginas.php");
 require_once(RUTA_PROYECTO . 'class/Productos_Especificaciones.php');
+require_once(RUTA_PROYECTO . 'class/Productos_Tallas.php');
 
 try {
 	$conexionBdComercial->query("DELETE FROM comercial_productos WHERE cprod_id='" . $_GET["id"] . "'");
@@ -12,6 +13,12 @@ try {
 		[
 			'cpt_id_producto' => $_GET["id"],
 			'cpt_tech_prin' => NO
+		]
+	);
+	Productos_Tallas::Delete(
+		[
+			'cpta_producto' => $_GET["id"],
+			'cpta_prin' => NO
 		]
 	);
 } catch (Exception $e) {
