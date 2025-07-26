0a637bb5b1a5fcc9273c83ab65bcd0a11e213ba8
ing-enuarlara <enuar2110@gmail.com>
2025-07-26 07:00:37 +0200

    Se muestra favicon

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ if (isset($_SESSION["id"])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminOCB 1.0 | Login</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="<?=REDIRECT_ROUTE?>files/logo/AdminZEFELogo.png">
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
