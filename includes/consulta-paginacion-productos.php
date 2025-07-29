3c3fc1bafdfbba4ea1a6db80d916b2b65dee4594
ing-enuarlara <enuar2110@gmail.com>
2025-07-29 22:15:31 +0200

    Se ordenan decendientemente

--- includes/consulta-paginacion-productos.php
+++ includes/consulta-paginacion-productos.php
@@ -12,7 +12,9 @@ $productosBD = Productos::SelectJoin(
         SubCategorias::class
     ],
     "LEFT JOIN " . Productos_Tallas::$schema . "." . Productos_Tallas::$tableName . " ON cpta_producto = cprod_id AND cpta_prin = 'NO'",
-    "cprod_id"
+    "cprod_id",
+    "",
+    "cprod_id DESC",
 );
 
 // APLICAR FILTROS
