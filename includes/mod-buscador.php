8368ca82109e61e2b2335369ae2898eddc4dd729
ing-enuarlara <enuar2110@gmail.com>
2025-08-05 18:05:38 +0200

    sE MANTIENE ENFOCADO EL BUSCADOR

--- includes/mod-buscador.php
+++ includes/mod-buscador.php
@@ -3,7 +3,7 @@
         <a href="<?=$_SERVER['PHP_SELF'];?>" class="btn btn-warning mr-5"> Quitar Filtro</a>
     <?php }?>
     <form class="form-inline" action="<?= $_SERVER['PHP_SELF']; ?>" method="get">
-        <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search" name="search" value="<?=$busqueda?>">
+        <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search" name="search" value="<?=$busqueda?>" autofocus>
         <button class="btn btn-info my-2 my-sm-0" type="submit">Buscar</button>
     </form>
 </div>
