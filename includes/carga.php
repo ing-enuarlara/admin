e3a6f993804e44e2e4855cbdc9282477b3846799
ing-enuarlara <enuar2110@gmail.com>
2025-08-25 17:18:40 +0200

    Se aumenta tamaño del logo de carga

--- includes/carga.php
+++ includes/carga.php
@@ -1,3 +1,3 @@
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="<?=REDIRECT_ROUTE?>files/logo/logo3.png" alt="AdminOCBLogo" height="230" width="200">
+    <img class="animation__shake" src="<?=REDIRECT_ROUTE?>files/logo/logo3.png" alt="AdminOCBLogo" height="500" width="500">
 </div>
